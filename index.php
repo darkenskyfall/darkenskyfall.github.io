3c5e6ba21c7003822df12321c6cfe2bf639c4de2
Dimas Pramudya Sumarsis <dimas.sumarsis@isocorp.co.id>
2020-09-17 23:31:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+  <title>Pramudya Dev</title>
+  <style>
+    body {
+      background: #050505;
+      color: #fff;
+      font-family: Arial;
+    }
+
+    .navbar {
+      padding: 22px;
+      background: #050505;
+    }
+
+    .navbar a {
+      color: #fff;
+    }
+
+    .hero {
+      /* Sizing */
+      width: 100%;
+      height: 100vh;
+
+      /* Flexbox stuff */
+      display: flex;
+      justify-content: left;
+      align-items: center;
+
+      /* Text styles */
+      text-align: left;
+    }
+
+    .hero-inner h1 {
+      font-weight: bold;
+      font-size: 40pt;
+    }
+
+    .hero-inner p {
+      font-size: 18pt;
+    }
+
+    .subtitle {
+      font-weight: bold;
+    }
+
+    .card-columns {
+      color: #050505;
+      font-weight: bold;
+    }
+
+    .card {
+      border-radius: 0px;
+      border:none;
+    }
+
+    .card img{
+      border-radius: 0px;
+    }
+
+    .rounded-circle{
+      width:30%;
+    }
+
+    .footer {
+      padding: 22px;
+      background: #050505;
+    }
+
+    .lead a{
+      color:#fff;
+    }
+  </style>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <a class="navbar-brand" href="#"><b>PRAMUDYAXYZ</b></a>
+  </nav>
+
+  <div class="container" style="margin-top:200px;">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <div class="hero-inner">
+          <h1>Halo, Saya Dimas <br>Pramudya Sumarsis</h1>
+          <p class="lead">Pengembang Perangkat Lunak berbasis di Surabaya, <br>sekarang di Xenopati!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" style="margin-top:200px;">
+    <div class="row">
+      <div class="col-md-8 mx-auto text-center">
+        <h1><b>Tentang Saya</b></h1>
+        <p class="lead">Salam Kenal! Saya Dimas Pramudya Sumarsis dari Surabaya, Mulai menjadi pengembang perangkat lunak sejak 4 tahun terakhir sejak duduk di bangku sekolah menengah kejuruan hingga saat ini bekerja di salah satu startup di kota pahlawan .</p>
+        <p class="lead">Pengembangan perangkat lunak meliputi membangun website dengan framework Laravel, membangun aplikasi berbasis iOS dengan Swift hingga uji coba . Untuk saat ini saya tertarik untuk mendalami bahasa pemrograman Python khususnya framework Django untuk membangun website .</p>
+        <img src="https://pramudyaxyz.files.wordpress.com/2019/10/img_20191014_111722_269-269811159.jpg" alt="..." class="rounded-circle mt-3">
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid" style="margin-top:200px;">
+    <div class="row">
+      <div class="col-md-12 mx-auto text-center">
+        <h1><b>Proyek</b></h1>
+        <div class="card-columns">
+          <div class="card text-left">
+            <img src="https://pramudyaxyz.files.wordpress.com/2020/07/safar-safarov-msn8tfhj0is-unsplash.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title">Pramudyastama</h2>
+              <p class="lead">Pramudyastama adalah sebuah website statis yang memuat berita seputar teknologi dan inovasi medio 2016</p>
+            </div>
+          </div>
+          <div class="card p-3">
+            <blockquote class="blockquote mb-0 card-body text-right">
+              <h2>Gubuktani.id</h2>
+              <p class="lead">Gubuktani merupakan proyek tugas akhir saya dalam menuntaskan masa SMK, Sebuah web dinamis dimana para pemilik sawah yang hendak menyewakan sawahnya untuk keperluan pertanian dan riset</p>
+              <footer class="blockquote-footer">
+                <small class="text-muted">
+                  Dibuat tahun <cite title="Source Title">2018</cite>
+                </small>
+              </footer>
+            </blockquote>
+          </div>
+          <div class="card">
+            <img src="https://pramudyaxyz.files.wordpress.com/2020/06/blur-close-up-code-computer-546819.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title">Suar Geguritan</h2>
+              <p class="lead">Kumpulan puisi berbahasa jawa dibuat dengan framework Django (Dalam Proses).</p>
+              <p class="card-text"><small class="text-muted">Dalam Tahap Pengembangan</small></p>
+            </div>
+          </div>
+          <div class="card bg-primary text-white text-center p-3">
+            <blockquote class="blockquote mb-0">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+              <footer class="blockquote-footer text-white">
+                <small>
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </small>
+              </footer>
+            </blockquote>
+          </div>
+          <div class="card text-center">
+            <div class="card-body">
+              <h2 class="card-title">Lorem Ipsum</h2>
+              <p class="lead">This card has a regular title and short paragraphy of text below it.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="https://pramudyaxyz.files.wordpress.com/2020/02/freshh-connection-m4lve6jr26e-unsplash.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="card p-3 text-right">
+            <blockquote class="blockquote mb-0">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+              <footer class="blockquote-footer">
+                <small class="text-muted">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </small>
+              </footer>
+            </blockquote>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title">Lorem Ipsum</h2>
+              <p class="lead">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container" style="margin-top:200px;">
+      <div class="row">
+        <div class="col-md-8 mx-auto text-center">
+          <h1><b>Jurnal</b></h1>
+          <p class="lead">Di waktu senggang saya biasa untuk menulis dan mengutarakan sesuatu yang berhubungan dengan hidup, teknologi dan pengalaman saya<br>semoga kamu terinspirasi ;)</p>
+          <a href="https://www.pramudyaxyz.wordpress.com" target="_blank" class="btn btn-outline-light">Baca Selengkapnya</a>
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer" style="margin-top:200px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+            <h3><b>Terima Kasih! <br>Jika ada yang perlu ditanyakan kamu bisa menghubungi saya</b></h3>
+            <h5 class="mt-5">Dimas Pramudya Sumarsis</h5>
+            <p class="lead">Pengembang Perangkat Lunak<br>Manukan, Surabaya<br>dimasrajawali76@gmail.com<br>+6285-881-824-590 (WhatsApp)</p>
+
+            <h5 class="mt-5">Sosial Media</h5>
+            <p class="lead"><a href="https://www.facebook.com/pramudyaxyz" target="_blank">Facebook</a>  <a href="https://www.twitter.com/pramudyaxyz" target="_blank">Twitter</a>  <a href="https://www.instagram.com/pramudyaxyz" target="_blank">Instagram</a>  <a href="https://www.linkedin.com/in/pramudyaxyz" target="_blank">Linkedin</a>  <a href="https://www.github.com/pramudyaxyz" target="_blank">Github</a></p>
+
+            <h5 class="mt-5"><b>&copy; PRAMUDYA SUMARSIS / 2020</b></h5>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
